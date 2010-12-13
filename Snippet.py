1432eec7c074c30dff7f1be44fcc513148b27db0
Andrew Hammond <andrew.george.hammond@gmail.com>
2010-12-13 11:04:56 -0800

    Applied PEP8 coding standards

--- Snippet.py
+++ Snippet.py
@@ -50,12 +50,12 @@ def highlight_doc(doc, query):
     '''
     return str(Snippet(doc, query))
 
-class Highlight_DocTests(unittest.TestCase):
+class HighlightDocTests(unittest.TestCase):
     def setUp(self):
         self.d = 'An example document with some fun and interesting words in it.'
         self.q = 'document fun and words'
 
-    def testBasic(self):
+    def test_go_right(self):
         self.assertEqual(
                 highlight_doc(self.d, self.q),
                 '... document with some [[HIGHLIGHT]]fun and[[ENDHIGHLIGHT]] interesting [[HIGHLIGHT]]words[[ENDHIGHLIGHT]] in it.')
@@ -78,21 +78,21 @@ class Token:
     def string(self):
         return self.__string__
 
-    def location(self):
+    def start_index(self):
         return self.__location__
 
-    def endLocation(self):
+    def end_index(self):
         return self.__endLocation__
 
 class TokenTests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testBasic(self):
+    def test_go_right(self):
         t = Token('a string', 24, 45)
         self.assertEqual(t.string(), 'a string')
-        self.assertEqual(t.location(), 24)
-        self.assertEqual(t.endLocation(), 45)
+        self.assertEqual(t.start_index(), 24)
+        self.assertEqual(t.end_index(), 45)
 
 
 class Tokenized:
@@ -108,28 +108,10 @@ class Tokenized:
     def __str__(self):
         return self.list.__str__()
 
-    def wordCount(self):
+    def word_count(self):
         if '__wordCount__' not in dir(self):
             self.list()
         return self.__wordCount__
-
-    def map(self):
-        '''Accessor function for a token map.
-        The token map is a dictionary that maps each normalized token to a list of it's locations in the document.
-        For example the map for 'A basic document'
-        '''
-        if '__map__' not in dir(self):
-            self.__map__ = {}
-            i = 0
-            for token in self.__tokenizer__.finditer(self.document):
-                name = __tokenNormalizer__(token.expand('\\1'))
-                if self.__map__.has_key(name):
-                    self.__map__[name].append(i)
-                else:
-                    self.__map__[name] = [i]
-                i += 1
-            self.__wordCount__ = i
-        return self.__map__
 
     def list(self):
         '''Accessor function for a token list.
@@ -146,33 +128,16 @@ class TokenizedTests(unittest.TestCase):
     def setUp(self):
         pass
 
-    def testMapBasic(self):
-        self.assertEqual(Tokenized('A basic document.').map(), {'a': [0], 'document': [2], 'basic': [1]})
-
-    def testMapNormalization(self):
-        self.assertEqual(Tokenized('A').map(), {'a': [0]})
-
-    def testMapApostrophe(self):
-        self.assertEqual(Tokenized("The apostrophe's usage isn't among the class' many issues.").map(),
-                {'among': [4], "apostrophe's": [1], "class'": [6], 'issues': [8], 'many': [7], 'usage': [2], 'the': [0, 5], "isn't": [3]}
-            )
-
-    def testMapPunctuation(self):    # Note: no apostrophe or underscore
-        self.assertEqual(Tokenized('!@#$%^&*()-=+\\|[]{};:"?/.>,<`~ ').map(), {})
-
-    def testMapMultipleInstance(self):
-        self.assertEqual(Tokenized('A, a and another a!').map(), {'a': [0, 1, 4], 'and': [2], 'another': [3]})
-
-    def testListBasic(self):
+    def test_list_go_right(self):
         self.assertEqual(Tokenized('A basic query').list(),
             [Token('a', 0, 1), Token('basic', 2, 7), Token('query', 8, 13)])
 
-    def testListMultipleInstance(self):
+    def test_list_multiple_instance(self):
         self.assertEqual(Tokenized('A, a and another a!').list(),
             [Token('a', 0, 1), Token('a', 3, 4), Token('and', 5, 8), Token('another', 9, 16), Token('a', 17, 18)])
 
     def testWordCount(self):
-        self.assertEqual(Tokenized('This is a six word sentence.').wordCount(), 6)
+        self.assertEqual(Tokenized('This is a six word sentence.').word_count(), 6)
 
 
 class Greatest:
@@ -192,14 +157,14 @@ class GreatestTests(unittest.TestCase):
     def setUp(self):
         self.g = Greatest(0)
 
-    def testInit(self):
+    def test_init_go_right(self):
         self.assertEqual(self.g.value(), 0)
 
-    def testAppendLarger(self):
+    def test_append_larger(self):
         self.g.append(5)
         self.assertEqual(self.g.value(), 5)
 
-    def testAppendSmaller(self):
+    def test_append_smaller(self):
         self.g.append(-5)
         self.assertEqual(self.g.value(), 0)
 
@@ -225,20 +190,20 @@ class SmithWatermanPointTest(unittest.TestCase):
     def setUp(self):
         self.p = SmithWatermanPoint(8, 'i')
 
-    def testWeight(self):
+    def test_weight(self):
         self.assertEqual(self.p.weight(), 8)
 
-    def testSource(self):
+    def test_source(self):
         self.assertEqual(self.p.source(), 'i')
 
-    def testCmp(self):
+    def test_cmp(self):
         smallerPoint = SmithWatermanPoint(4, 'd')
         self.assertTrue(smallerPoint < self.p)
 
 
 class SmithWatermanMatrix:
-    weightMatch  =  2        # a match is worth 2 points
-    weightMiss   = -1        # a mismatch costs 1
+    weight_match  =  2        # a match is worth 2 points
+    weight_miss   = -1        # a mismatch costs 1
     dereferenceNode = {
             'm' : lambda x: (x[0]-1, x[1]-1),           # match or miss, go diagonally left and up
             'i' : lambda x: (x[0]  , x[1]-1),           # inserted a token, go left
@@ -249,51 +214,51 @@ class SmithWatermanMatrix:
         '''Given a document and a query as Tokenized objects, initialize a matrix of tupels to zero.'''
         self.document = document.list()
         self.query = query.list()
-        self.documentLen = document.wordCount() + 1
-        self.queryLen = query.wordCount() + 1
-        self.__matrix__ = [0] * self.documentLen
-        for d in xrange(0, self.documentLen):
-            self.__matrix__[d] = [0] * self.queryLen
-            for q in xrange(0, self.queryLen):
+        self.document_length = document.word_count() + 1
+        self.query_length = query.word_count() + 1
+        self.__matrix__ = [0] * self.document_length
+        for d in xrange(0, self.document_length):
+            self.__matrix__[d] = [0] * self.query_length
+            for q in xrange(0, self.query_length):
                 self.__matrix__[d][q] = SmithWatermanPoint(0,' ')
-        self.__highestWeight__ = SmithWatermanPoint(0, ' ')
-        self.__highestLocation__ = (0,0)
+        self.__highest_weight__ = SmithWatermanPoint(0, ' ')
+        self.__highest_location__ = (0,0)
 
     def __str__(self):
         '''A textual representation of the SW matrix.
         Across the top we have the query, and down the side, we have the document.
         A more traditional representation have the query across the top.
         '''
-        documentOffset = max(map(lambda a: len(a.string()), self.document))    # find the longest term in the document
-        queryOffset = max(map(lambda a: len(a.string()), self.query))    + 1    # find longest term in the search
+        document_offset = max(map(lambda a: len(a.string()), self.document))    # find the longest term in the document
+        query_offset = max(map(lambda a: len(a.string()), self.query))    + 1    # find longest term in the search
         # If the largest query term is smaller than the len(r", (1,'m')"), default to that length.
         # Obviously this won't work very well for weights beyond 9 because it assumes a single digit.
         # TODO: support arbitrary weights
-        if queryOffset < 10:
-            queryOffset = 10
-        s = ' ' * documentOffset                                         # header string
+        if query_offset < 10:
+            query_offset = 10
+        s = ' ' * document_offset                                         # header string
         # first query term is the "-" term (no match)
-        s += ' ' * (queryOffset - 2) + '-'
+        s += ' ' * (query_offset - 2) + '-'
         for q in self.query:
-            s += ' ' * (queryOffset - len(q.string())) + q.string()
+            s += ' ' * (query_offset - len(q.string())) + q.string()
         s += "\n"
         for i in xrange(len(self.__matrix__)):
             d = self.document[i-1].string() if i > 0 else '-'
-            s += ' ' * (documentOffset - len(d)) + d + str(self.__matrix__[i]) + " %d\n" % i
+            s += ' ' * (document_offset - len(d)) + d + str(self.__matrix__[i]) + " %d\n" % i
         return s
 
     #TODO: render to html for better readability
 
     def dimensions(self):
-        return (self.documentLen, self.queryLen)
+        return self.document_length, self.query_length
 
     def highestWeight(self):
         self.heat()
-        return self.__highestWeight__
+        return self.__highest_weight__
 
     def highestLocation(self):
         self.heat()
-        return self.__highestLocation__
+        return self.__highest_location__
 
     def __matchWeight__(self, d, q):
         '''Boolean text match.
@@ -302,7 +267,7 @@ class SmithWatermanMatrix:
         # Maybe a BLOSUM type approach based on the kind of language involved?
         # For example, you could give a partial match value from pizza to pie.
         # This doesn't of course address the issue of mapping from one term to phrasal terms.
-        return self.weightMatch if self.document[d-1].string() == self.query[q-1].string() else self.weightMiss
+        return self.weight_match if self.document[d-1].string() == self.query[q-1].string() else self.weight_miss
 
     def heat(self):
         '''An accessor function that generates the SW heat matrix.
@@ -310,33 +275,33 @@ class SmithWatermanMatrix:
         Then, going left to right, from top to bottome and starting at the 1th index (not the 0th)
         We take the greatest of 0, the diagonally
         '''
-        if '__hasBeenHeated__' not in dir(self):
+        if '__heated__' not in dir(self):
             m = self.__matrix__
-            for d in xrange(1, self.documentLen):                   # leave the 0th index elements at 0: guarantee
-                for q in xrange(1, self.queryLen):                  # optimalPath search below finds an end point
+            for d in xrange(1, self.document_length):                   # leave the 0th index elements at 0: guarantee
+                for q in xrange(1, self.query_length):                  # optimalPath search below finds an end point
                     match = self.__matchWeight__(d,q)
                     greatest = Greatest(SmithWatermanPoint(0, ' ')) # Omit this for a global match rather than local
                     greatest.append(SmithWatermanPoint(m[d - 1][q-1].weight() + match, 'm'))
                     greatest.append(SmithWatermanPoint(m[d - 1][q].weight() + match, 'd'))
                     greatest.append(SmithWatermanPoint(m[d][q - 1].weight() + match, 'i'))
                     self.__matrix__[d][q] = greatest.value()
-                    if greatest.value() > self.__highestWeight__:
-                        self.__highestWeight__ = greatest.value()
-                        self.__highestLocation__ = (d,q)
-            self.__hasBeenHeated__ = 1
+                    if greatest.value() > self.__highest_weight__:
+                        self.__highest_weight__ = greatest.value()
+                        self.__highest_location__ = (d,q)
+            self.__heated__ = 1
 
     def optimalPath(self):
         if '__path__' not in dir(self):
             self.heat()            # can't path an un-heated matrix.
             # starting at the highest location, path the matrix first up and left, then down and right.
             self.__path__ = list()
-            currentAddress = self.__highestLocation__
-            currentPoint = self.__matrix__[currentAddress[0]][currentAddress[1]]
-            while currentPoint.weight() > 0:
-                if currentPoint.source() == 'm':
-                    self.__path__.insert(0, currentAddress[0])
-                currentAddress = self.dereferenceNode[currentPoint.source()](currentAddress)
-                currentPoint = self.__matrix__[currentAddress[0]][currentAddress[1]]
+            current_address = self.__highest_location__
+            current_point = self.__matrix__[current_address[0]][current_address[1]]
+            while current_point.weight() > 0:
+                if current_point.source() == 'm':
+                    self.__path__.insert(0, current_address[0])
+                current_address = self.dereferenceNode[current_point.source()](current_address)
+                current_point = self.__matrix__[current_address[0]][current_address[1]]
         return self.__path__
 
 class SmithWatermanMatrixTests(unittest.TestCase):
@@ -384,10 +349,10 @@ class SmithWatermanMatrixTests(unittest.TestCase):
 class Snippet:
     '''A class to support the management and generation of snippets.'''
 
-    __snippetMaxHead__ = 20
-    __snippetMaxTail__ = 20
-    __highlightStart__ = '[[HIGHLIGHT]]'
-    __highlightEnd__   = '[[ENDHIGHLIGHT]]'
+    __snippet_max_head__ = 20
+    __snippet_max_tail__ = 20
+    __highlight_start__ = '[[HIGHLIGHT]]'
+    __highlight_end__   = '[[ENDHIGHLIGHT]]'
 
     def __init__(self, document, query):
         self.document = Tokenized(document)
@@ -404,13 +369,12 @@ class Snippet:
             self.__matrix__ = SmithWatermanMatrix(self.document, self.query)
         return self.__matrix__
 
-    def highlightSpans(self):
+    def highlight_spans(self):
         '''Accessor function that constructs a list of tuples that represent the beginning and end of highlight sections.
         '''
         if '__highlights__' not in dir(self):
             self.__highlights__ = []
             terms = deque(self.matrix().optimalPath())
-            lastTerm = None
             try:
                 # remember, matrix indexing is +1 from Tokenized list indexing
                 t = terms.popleft() - 1
@@ -424,23 +388,23 @@ class Snippet:
                 pass
         return self.__highlights__
 
-    def startIndexFromTerm(self, termNumber):
+    def start_index(self, termNumber):
         '''Given a term in the document.list(), return it's start index
         '''
-        return self.document.list()[termNumber].location()
+        return self.document.list()[termNumber].start_index()
 
-    def endIndexFromTerm(self, termNumber):
+    def end_index(self, termNumber):
         '''Geven a term in the document.list(), return it's end index
         '''
-        return self.document.list()[termNumber].endLocation()
+        return self.document.list()[termNumber].end_index()
 
-    def hasMatch(self):
+    def has_match(self):
         return 0 < len(self.matrix().optimalPath())
 
     def __str__(self):
         '''Return a highlited snippet.
         '''
-        spans = deque(self.highlightSpans())
+        spans = deque(self.highlight_spans())
         d = self.document.document
         r = ''
         cursor = 0
@@ -448,19 +412,19 @@ class Snippet:
         if len(spans) > 0:
             peekStart, peekEnd = spans.popleft()
             spans.appendleft((peekStart, peekEnd))
-            cursor = self.startIndexFromTerm(peekStart) - self.__snippetMaxHead__
+            cursor = self.start_index(peekStart) - self.__snippet_max_head__
             if cursor > 0:
                 r = '...'
             else:
                 cursor = 0
         while len(spans):
             startTerm, endTerm = spans.popleft()
-            start = self.startIndexFromTerm(startTerm)
-            end = self.endIndexFromTerm(endTerm)
-            r += d[cursor:start] + self.__highlightStart__ + d[start:end] + self.__highlightEnd__
+            start = self.start_index(startTerm)
+            end = self.end_index(endTerm)
+            r += d[cursor:start] + self.__highlight_start__ + d[start:end] + self.__highlight_end__
             cursor = end
         # TODO: like the head, should the tail break on word boundaries instead
-        tailLength = cursor+self.__snippetMaxTail__
+        tailLength = cursor+self.__snippet_max_tail__
         if tailLength > len(d):
             r+=d[cursor:]
         else:
@@ -471,31 +435,34 @@ class SnippetTests(unittest.TestCase):
     def setUp(self):    #      0       1    2    3     4    5  6         7
         self.s = Snippet('Little star\'s deep dish pizza sure is fantastic.', 'deep dish pizza')
 
-    def testHasMatchTrue(self):
-        self.assertTrue(self.s.hasMatch())
+    def test_has_match_True(self):
+        self.assertTrue(self.s.has_match())
 
-    def testHasMatchFalse(self):
+    def test_has_match_False(self):
         s = Snippet('No match', 'foo')
-        self.assertFalse(s.hasMatch())
+        self.assertFalse(s.has_match())
 
-    def testHighlightSpans(self):
-        self.assertEqual(self.s.highlightSpans(), [[2,4]])
+    def test_highlight_spans_go_right(self):
+        self.assertEqual(self.s.highlight_spans(), [[2,4]])
 
-    def testHighlightSpansComplex(self):
+    def test_highlight_spans_go_right_complex(self):
         s = Snippet('A big complicated document with some complicated stuff in it.', 'with some stuff')
-        self.assertEqual(s.highlightSpans(), [[4,5],[7,7]])
+        self.assertEqual(s.highlight_spans(), [[4,5],[7,7]])
 
-    def testIndexFromTerm(self):
-        self.assertEqual(self.s.startIndexFromTerm(3), 19)
+    def test_start_index(self):
+        self.assertEqual(self.s.start_index(3), 19)
 
-    def testStr(self):
+    def test_end_index(self):
+        self.assertEqual(self.s.end_index(3), 23)
+
+    def test_str(self):
         self.assertEqual(str(self.s), 'Little star\'s [[HIGHLIGHT]]deep dish pizza[[ENDHIGHLIGHT]] sure is fantastic.')
 
-    def testStrComplex(self):
+    def test_str_complex(self):
         s = Snippet('A big complicated document with some complicated stuff in it.', 'with some stuff')
         self.assertEqual(str(s), '...omplicated document [[HIGHLIGHT]]with some[[ENDHIGHLIGHT]] complicated [[HIGHLIGHT]]stuff[[ENDHIGHLIGHT]] in it.')
 
-    def testStrLongTail(self):
+    def test_str_long_tail(self):
         s = Snippet('A big complicated document with some complicated stuff in it.', 'big')
         self.assertEqual(str(s), 'A [[HIGHLIGHT]]big[[ENDHIGHLIGHT]] complicated documen...')
 
